c3578b72ebcec9b91f91d4a4d862986635104531
Pedro Arns Junior <pedroarnsjr@outlook.com>
2023-05-09 20:42:08 -0300

    Project created

---
+++ dslist/src/test/java/com/devsuperiro/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiro.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
