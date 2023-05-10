e5f8d8a98c6726f34ea67c30c70c1b8c4b7d5a9d
Pedro Arns Junior <pedroarnsjr@outlook.com>
2023-05-09 23:32:46 -0300

    properties, Game, ORM

--- dslist/src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ dslist/src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperiro.dslist;
+package com.devsuperior.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
